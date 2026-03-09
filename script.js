062321442a740c9c0538bd0cf134d37130adda5b
AdrianaJ <104113099+Adr132@users.noreply.github.com>
2026-03-09 16:11:15 -0600

    portafolio1.5

--- script.js
+++ script.js
@@ -4,11 +4,3 @@ function toggleMenu (){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-function scrollFunction () {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-}
